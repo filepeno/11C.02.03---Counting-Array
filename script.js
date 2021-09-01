04c34b83e5eb5ab819b13bf54b63a8dbeaa723f7
Filip <fili4252@stud.kea.dk>
2021-09-01 23:37:14 +0200

    made the array and keep it's length to 9

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 "use strict";
 
 let number = 0;
+const numArr = [0];
 
 count();
 
 function count() {
-  console.log(number);
+  console.log(numArr);
   number++;
+  numArr.unshift(number);
   setTimeout(count, 1000);
+  if (number > 9) {
+    numArr.length = 9;
+  }
 }
